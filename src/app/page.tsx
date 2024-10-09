f1fc656f216dcceee35b523b3bea1f72e5834196
Nwosu Chidinma <chidinmaifynwosu@gmail.com>
2024-10-09 13:01:46 +0100

    Omitted "default" from the export, had to add it in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Home from '../app/(main)/home/page';
 
-export function Main():React.JSX.Element {
+export default function Main():React.JSX.Element {
   return (
     <>
     <div className="min-h-screen">
